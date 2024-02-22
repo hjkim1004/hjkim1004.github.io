2a43f2b89c9ede8718d49a78799efbe31f89e56a
hjkim1004 <developer.heejeong@gmail.com>
2024-02-23 06:21:57 +0900

    [2024.02.23] 메인페이지 구상

--- src/components/layout/flop/index.tsx
+++ src/components/layout/flop/index.tsx
@@ -1,17 +1,37 @@
 import React, {useEffect} from 'react';
-import {IconButton} from "@mui/material";
-import {FaMessage} from "react-icons/fa6";
+import {SiVelog} from "react-icons/si";
+import {RiKakaoTalkFill} from "react-icons/ri";
+import {FaGithub} from "react-icons/fa";
 
 const FlopMenu = () => {
-    const kakaoChannelButtonClick = () => {
-        window.location.href = 'http://pf.kakao.com/_byerG/chat';
-    }
-
     return (
-        <div className="flopmenu">
-            <IconButton onClick={kakaoChannelButtonClick}>
-                <FaMessage />
-            </IconButton>
+        <div className="flop-list">
+            <ul className="flop-menu">
+                <li className="flop-item" data-name="kakao">
+                    <a href="http://pf.kakao.com/_byerG/chat" target="_blank">
+                        <div className="icon">
+                            <RiKakaoTalkFill />
+                        </div>
+                        <div className="name">카톡 채널</div>
+                    </a>
+                </li>
+                <li className="flop-item" data-name="blog">
+                    <a href="https://velog.io/@developer_khj" target="_blank">
+                        <div className="icon">
+                            <SiVelog />
+                        </div>
+                        <div className="name">블로그</div>
+                    </a>
+                </li>
+                <li className="flop-item" data-name="github">
+                    <a href="http://github.com/hjkim1004" target="_blank">
+                        <div className="icon">
+                            <FaGithub />
+                        </div>
+                        <div className="name">깃허브</div>
+                    </a>
+                </li>
+            </ul>
         </div>
     );
 };
--- src/pages/main/section/home.tsx
+++ src/pages/main/section/home.tsx
@@ -5,7 +5,9 @@ const HomeSection = () => {
         <section className="section all-center" id="section_home">
             <div className="section-bg"></div>
 
-            <h1 className="section-title">Developer Twinkle's Portfolio</h1>
+            <h1 className="section-title">
+                <div className="typing">Developer Twinkle's Portfolio</div>
+            </h1>
 
         </section>
     );
