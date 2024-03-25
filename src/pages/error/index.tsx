fa845de74651dfeeee293af1523ca5605a190f6d
hjkim1004 <developer.heejeong@gmail.com>
2024-03-25 21:14:06 +0900

    [2024.03.25] Error 페이지 title 변경

--- src/pages/error/index.tsx
+++ src/pages/error/index.tsx
@@ -1,7 +1,12 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import NotFound from "@Pages/error/404";
+import * as process from "process";
 
 const App = () => {
+    useEffect(() => {
+        document.title = process.env.TITLE +  " | 404 NOT FOUND";
+    }, []);
+
     return (
         <>
             <main id="content">
