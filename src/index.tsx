c2021bb93e2a0c3bd1451835663dba872a4148cc
hjkim1004 <developer.heejeong@gmail.com>
2024-03-25 21:15:32 +0900

    [2024.03.25] Noto Sans KR 로컬로 적용

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import 'tippy.js/dist/tippy.css';
-import '@Fonts/notosans.css'
+import '@Fonts/notokr/notokr.css'
 import '@Fonts/default.css'
 import '@Style/style.css';
 import '@Style/light.css';
